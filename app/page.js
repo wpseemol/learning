a7d4c947e9f3fdf75aae56ac6cae65203ee0ae6a
wpseemol <wpseemol@gmail.com>
2024-07-11 20:24:25 +0600

    image zoom file created

--- app/page.js
+++ app/page.js
@@ -1,113 +1,164 @@
-import Image from "next/image";
+'use client';
+
+import imageUrl from '@/public/sample.avif';
+import { useZoomImageHover, useZoomImageWheel } from '@zoom-image/react';
+import Image from 'next/image';
+import { useEffect, useMemo, useRef, useState } from 'react';
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    const [tabs, setTabs] = useState([
+        { name: 'Wheel', href: '#', current: true, value: 'wheel' },
+        { name: 'Hover', href: '#', current: false, value: 'hover' },
+        { name: 'Move', href: '#', current: false, value: 'move' },
+        { name: 'Click', href: '#', current: false, value: 'click' },
+    ]);
+
+    const zoomType = useMemo(
+        () => tabs.find((tab) => tab.current)?.value,
+        [tabs]
+    );
+
+    console.log(zoomType);
+
+    const imageWheelContainerRef = useRef(null);
+    const imageHoverContainerRef = useRef(null);
+    // const imageMoveContainerRef = useRef(null);
+    const imageClickContainerRef = useRef(null);
+    const zoomTargetRef = useRef(null);
+    const [croppedImage, setCroppedImage] = useState(null);
+
+    const {
+        createZoomImage: createZoomImageWheel,
+        zoomImageState: zoomImageWheelState,
+        setZoomImageState: setZoomImageWheelState,
+    } = useZoomImageWheel();
+    const { createZoomImage: createZoomImageHover } = useZoomImageHover();
+
+    const makeHandleTabClick = (tab) => () => {
+        setTabs(
+            tabs.map((t) => {
+                if (t.name === tab.name) {
+                    return { ...t, current: true };
+                } else {
+                    return { ...t, current: false };
+                }
+            })
+        );
+    };
+
+    const croppedImageClasses = useMemo(() => {
+        if (zoomImageWheelState.currentRotation % 180 === 90) {
+            return 'h-[200px] w-[300px]';
+        } else {
+            return 'h-[300px] w-[200px]';
+        }
+    }, [zoomImageWheelState.currentRotation]);
+
+    useEffect(() => {
+        setCroppedImage('');
+
+        if (zoomType === 'hover') {
+            const imageContainer = imageHoverContainerRef.current;
+            const zoomTarget = zoomTargetRef.current;
+            createZoomImageHover(imageContainer, {
+                zoomImageSource: '/sample.avif',
+                customZoom: { width: 300, height: 500 },
+                zoomTarget,
+                scale: 2,
+            });
+        }
+    }, [zoomType]);
+
+    function zoomInWheel() {
+        setZoomImageWheelState({
+            currentZoom: zoomImageWheelState.currentZoom + 0.5,
+        });
+    }
+
+    function zoomOutWheel() {
+        setZoomImageWheelState({
+            currentZoom: zoomImageWheelState.currentZoom - 0.5,
+        });
+    }
+
+    function rotate() {
+        setZoomImageWheelState({
+            currentRotation: zoomImageWheelState.currentRotation + 90,
+        });
+    }
+
+    return (
+        <div className="p-4 font-sans">
+            <nav className="flex space-x-4 pb-4" aria-label="Tabs">
+                <button onClick={makeHandleTabClick('hover')}>hover</button>
+                {tabs.map((tab) => {
+                    return (
+                        <a
+                            onClick={makeHandleTabClick(tab)}
+                            key={tab.name}
+                            href={tab.href}
+                            className={
+                                'decoration-none rounded-md px-3 py-2 text-sm font-medium ' +
+                                (tab.current
+                                    ? 'bg-gray-100 text-gray-700'
+                                    : 'text-gray-500 hover:text-gray-700')
+                            }
+                            aria-current={tab.current ? 'page' : undefined}>
+                            {tab.name}
+                        </a>
+                    );
+                })}
+            </nav>
+
+            {zoomType === 'wheel' && (
+                <div className="space-y-4">
+                    <p>
+                        Current zoom:{' '}
+                        {`${Math.round(
+                            zoomImageWheelState.currentZoom * 100
+                        )}%`}
+                    </p>
+                    <p>Scroll inside the image to see zoom in-out effect</p>
+                    <div className="flex items-center gap-4">
+                        <div className="mt-1 grid h-[300px] w-[300px] place-content-center bg-black">
+                            <div
+                                ref={imageWheelContainerRef}
+                                className="h-[300px] w-[200px] cursor-crosshair duration-500">
+                                <Image
+                                    className="h-full w-full"
+                                    alt="Large Pic"
+                                    src={imageUrl}
+                                />
+                            </div>
+                        </div>
+                        {croppedImage && (
+                            <Image
+                                src={croppedImage}
+                                className={croppedImageClasses}
+                                alt="Cropped placeholder"
+                            />
+                        )}
+                    </div>
+                </div>
+            )}
+
+            {zoomType === 'hover' && (
+                <>
+                    <p>Hover inside the image to see zoom effect</p>
+                    <div
+                        ref={imageHoverContainerRef}
+                        className="relative flex h-[300px] w-[200px] items-start">
+                        <Image
+                            className="h-full w-full"
+                            alt="Small Pic"
+                            src={imageUrl}
+                        />
+                        <div
+                            ref={zoomTargetRef}
+                            className="absolute left-[350px]"></div>
+                    </div>
+                </>
+            )}
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+    );
 }
