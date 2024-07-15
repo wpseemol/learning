30cce793b6d345136a22594930ab8ce66e63fb7c
wpseemol <wpseemol@gmail.com>
2024-07-15 21:39:56 +0600

    drak and drop pakeg use here

--- app/upload/_component/upload-component.tsx
+++ app/upload/_component/upload-component.tsx
@@ -1,24 +1,50 @@
+'use client';
+
 import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
+import React from 'react';
+import { useDropzone } from 'react-dropzone';
 
 export default function UploadComponent() {
-    async function submitAction(formData: FormData) {
-        'use server';
-        const data = {
-            picture: formData.get('picture'),
-        };
+    const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
 
-        console.log(data);
+    const files = acceptedFiles?.map((file) => (
+        <li key={file.path}>
+            {file.path} - {file.size} bytes.
+        </li>
+    ));
+
+    async function submitAction(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+
+        const formData = new FormData(event.currentTarget);
+        acceptedFiles.forEach((file, index) => {
+            formData.append(`picture`, file);
+        });
+
+        // formData.forEach((file) => {
+        //     console.log(file);
+        // });
+
+        console.log(formData.getAll('picture'));
     }
 
     return (
-        <form action={submitAction}>
-            <div>
-                <label htmlFor="picture"></label>
-                <Input id="picture" name="picture" type="file" required />
-            </div>
-            <br />
-            <Button type="submit">Submit</Button>{' '}
-        </form>
+        <>
+            <aside className="">
+                <ul>{files}</ul>
+            </aside>
+
+            <form onSubmit={submitAction}>
+                <div
+                    {...getRootProps({
+                        className: 'border-dashed border-2 border-sky-500 p-4',
+                    })}>
+                    <input {...getInputProps()} />
+                    <p>Drag drop some files here, or click to select files</p>
+                </div>
+                <br />
+                <Button type="submit">Submit</Button>{' '}
+            </form>
+        </>
     );
 }
--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -2,7 +2,7 @@ import UploadComponent from './_component/upload-component';
 
 export default function UploadPage() {
     return (
-        <main className="main flex justify-center items-center h-screen">
+        <main className="main flex flex-col justify-center items-center h-screen">
             <UploadComponent />
         </main>
     );
