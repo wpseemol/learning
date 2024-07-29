0a25efaea94c8cf30d0edda97d399b9b0e88eb44
wpseemol <wpseemol@gmail.com>
2024-07-29 21:58:30 +0600

    pyton mathematical oparator learning

---
+++ first/mathematic.py
@@ -0,0 +1,14 @@
+print("addition:");
+print(1+1);
+
+print("subtraction:");
+print(4-2);
+print("multiply:");
+print(2*2);
+print("division:")
+print(6/3);
+print("division:")
+print(6//3);
+
+print("modulus:");
+print("10%3");
