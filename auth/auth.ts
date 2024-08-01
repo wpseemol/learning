c137e991c47e37a0ed147f5b513d3d06b6490e19
wpseemol <wpseemol@gmail.com>
2024-08-01 14:20:46 +0600

    google provider add on my next auth

---
+++ auth/auth.ts
@@ -0,0 +1,17 @@
+import NextAuth from 'next-auth';
+
+import Google from 'next-auth/providers/google';
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+    providers: [
+        Google({
+            authorization: {
+                params: {
+                    prompt: 'consent',
+                    access_type: 'offline',
+                    response_type: 'code',
+                },
+            },
+        }),
+    ],
+});
