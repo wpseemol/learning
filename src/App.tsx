8002b9b13c46e52646d719e5c7ee60121d7255b5
wpseemol <wpseemol@gmail.com>
2024-08-11 21:24:46 +0600

    fremar moton install and then again set moson in togle

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import SideMenu from './components/side-menu';
+
+import { ListItem } from './types';
+
+const data: ListItem[] = [
+    { name: '1', url: '/private/loans' },
+    { name: '2', url: '/private/cards' },
+    { name: '3', url: '/private/deposits' },
+    { name: '4', url: '/private/services' },
+    { name: '5', url: '/private/services' },
+    { name: '6', url: '/private/services' },
+    { name: '7', url: '/private/services' },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <SideMenu overlayColor="#303030" data={data} width={300} />
+        </>
+    );
 }
 
-export default App
+export default App;
