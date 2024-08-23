cb2d1dd5d9d4d221fd62fd958856e47261f84f08
wpseemol <wpseemol@gmail.com>
2024-08-23 22:44:53 +0600

    npm init -y and then create index file thaen type redex type and conestent file

---
+++ index.js
@@ -0,0 +1,37 @@
+// defining constants
+
+const INCREMENT = 'INCREMENT';
+const DECREMENT = 'DECREMENT';
+const ADD_USER = 'ADD_USER';
+
+// state
+const initialCounterState = {
+    count: 0,
+};
+
+const initialUsersState = {
+    users: [{ name: 'seemol chakroborti' }],
+};
+
+// action - object type, payload
+
+const incrementCounter = () => {
+    return {
+        type: INCREMENT,
+    };
+};
+
+const decrementCounter = () => {
+    return {
+        type: DECREMENT,
+    };
+};
+
+const addUser = () => {
+    return {
+        type: ADD_USER,
+        payload: {
+            name: 'Sujon mondol',
+        },
+    };
+};
