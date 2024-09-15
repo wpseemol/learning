bb9062de962c2e3ae0d09e3e26c18af3cac6ce5a
wpseemol <wpseemol@gmail.com>
2024-09-15 15:12:20 +0600

    learning fremar moston file keep learning repo

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,10 @@
 import './App.css';
 import SideMenu from './components/side-menu';
 
-import { ListItem } from './types';
-
-const data: ListItem[] = [
-    { name: '1', url: '/private/loans' },
-    { name: '2', url: '/private/cards' },
-    { name: '3', url: '/private/deposits' },
-    { name: '4', url: '/private/services' },
-    { name: '5', url: '/private/services' },
-    { name: '6', url: '/private/services' },
-    { name: '7', url: '/private/services' },
-];
-
 function App() {
     return (
         <>
-            <SideMenu overlayColor="#303030" data={data} width={300} />
+            <SideMenu />
         </>
     );
 }
