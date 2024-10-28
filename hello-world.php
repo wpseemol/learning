079095c8264687fc30c9f5b8ce02e03183e03c7f
wpseemol <wpseemol@gmail.com>
2024-10-28 15:02:35 +0600

    variable defind constant variable and regular variable

--- hello-world.php
+++ hello-world.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php hello world</title>
+</head>
+<body>
+
 <?php 
 
  echo "Hello world </br>";
@@ -5,4 +14,10 @@
  print("hello world </br>");
 
  printf( "hello world");
+
+ ?>   
+
+</body>
+</html>
+
 
---
+++ variable.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php variable</title>
+</head>
+<body>
+    <?php
+    $name = "seemol chakroborti";
+
+    echo $name;
+
+    echo "</br>";
+
+    define("MY_NAME","Seemol chakroborti");
+    
+    echo MY_NAME;
+
+    /**
+     * php comment like this.
+     */
+
+    ?>
+</body>
+</html>
