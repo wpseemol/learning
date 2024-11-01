0238188dee5e37383f05e7c9fbb01995d88f12fb
wpseemol <wpseemol@gmail.com>
2024-11-01 08:15:55 +0600

    learning wordpress theme devlepment

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Main template file.
+ * 
+ * @package wpseemol
+ */
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>wpSeemol</title>
+</head>
+
+<body>
+    hello wordpress.
+</body>
+
+</html>
