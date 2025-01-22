be4216ac5fccfcb311a71430e69636e44056aa40
wpseemol <wpseemol@gmail.com>
2025-01-22 19:02:24 +0600

    very basick node image upload

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.json({
+        message: 'hello express ssss',
+    });
 });
 
 app.listen(port, () => {
