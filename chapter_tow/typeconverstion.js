ce77cb112e5b28f3cac8005708442865a266fac2
Seemol Chakroborti <wpseemol>
2025-03-25 22:46:25 +0600

    parseInt and Number() consotrotor method and Infinity declear

---
+++ chapter_tow/typeconverstion.js
@@ -0,0 +1,16 @@
+let numberString = '1000';
+let n = 10;
+
+//console.log(numberString * n); // auto methik typeconverstion.
+
+//console.log(numberString + n); // string concat data.
+
+let number = Number(numberString); // string to number convart
+let number2 = Number.parseInt(numberString); // string to number convart
+let number3 = parseInt(numberString);
+
+//console.log(number);
+//console.log(number2);
+//console.log(number3);
+
+console.log(Infinity); // Infinity is js type deffrent type.
