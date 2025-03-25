3d028d880017495e245ba29866787aa903521b7c
Seemol Chakroborti <wpseemol>
2025-03-25 23:53:34 +0600

    math random number and  some use full mathod try

--- chapter_tow/math.js
+++ chapter_tow/math.js
@@ -1,4 +1,22 @@
 const e = Math.E;
 const piValue = Math.PI;
 
-console.log(piValue);
+const number = 2.234;
+
+const abs = Math.abs(number);
+
+const floorValue = Math.floor(number);
+const ceilValue = Math.ceil(number);
+const roundValue = Math.round(number);
+
+const mathMin = Math.min(200, 100, 50, 300);
+
+const mathMax = Math.max(200, 100, 490, 200);
+
+const numberPower = Math.pow(5, 2);
+
+const squerRoot = Math.sqrt(64);
+
+const randomNumber = Math.random();
+
+console.log('randomNumber:', randomNumber);
