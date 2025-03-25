c8da9b129ee0e2901b6289812bd0bdc493c7a63b
Seemol Chakroborti <wpseemol>
2025-03-25 23:04:30 +0600

    javascript falsy values list

--- chapter_tow/typeconverstion.js
+++ chapter_tow/typeconverstion.js
@@ -13,6 +13,22 @@ let number3 = parseInt(numberString);
 //console.log(number2);
 //console.log(number3);
 
-console.log(Infinity); // Infinity is js type deffrent type.
+//console.log(Infinity); // Infinity is js type deffrent type.
 
-console.log(typeof Infinity); // Infinity is number type.
+//console.log(typeof Infinity); // Infinity is number type.
+//console.log(typeof Infinity.toString()); // Infinity type change string.
+//console.log(typeof String(Infinity)); // Infinity type change string.
+
+const infinityBoolean = Boolean(Infinity); //Infinity type change to boolean type it's true value.
+
+// console.log(infinityBoolean);
+
+/**
+ * Falsy Values
+ * ""
+ * 0
+ * null
+ * undefined
+ * NaN
+ *
+ */
