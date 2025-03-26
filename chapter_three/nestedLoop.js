8e6378df9b8ced6291dd29ed067d91edb69b181a
Seemol Chakroborti <wpseemol>
2025-03-26 14:27:00 +0600

    use for loop make rectengel

---
+++ chapter_three/nestedLoop.js
@@ -0,0 +1,27 @@
+
+let n = 2
+
+console.log("use for loop make square")
+for(i = 0; i < n; i++){
+    let result = ""
+    for(j = 0; j < n; j++){
+        result += "+ "
+    }
+    console.log(result)
+}
+
+
+
+console.log("use for loop make triangle")
+let n2 = 5;
+
+for(i = 0; i < n2; i++){
+    let result = ""
+    for(j = 0; j < i; j++){
+        result += "+ "
+    }
+    console.log(result)
+}
+
+
+
