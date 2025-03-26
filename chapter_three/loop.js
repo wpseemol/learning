3b065c45d257920b09438b4d4a1b39631ef0112f
Seemol Chakroborti <wpseemol>
2025-03-26 14:02:16 +0600

    while loop is create

--- chapter_three/loop.js
+++ chapter_three/loop.js
@@ -1,7 +1,5 @@
 /**
- * Loop are statements which Executes some bolck of code repeatedly until the condisions false.
- * helo 
- * 
+ * Loop are statements which Executes some bolck of code repeatedly until the condisions false. 
  */
 
 for(i=0; i<5; i++){
