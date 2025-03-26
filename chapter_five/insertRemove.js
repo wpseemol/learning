74871f9ad83dce8d5458e23f0521fa242b447bc1
Seemol Chakroborti <wpseemol>
2025-03-27 00:23:13 +0600

    array shift and unshift, push, pop

---
+++ chapter_five/insertRemove.js
@@ -0,0 +1,14 @@
+
+const frutes = ['apple', 'banana', 'orange', 'grape', 'kiwi'];
+
+frutes.push('pear');
+
+console.log(frutes); // ['apple', 'banana', 'orange', 'grape', 'kiwi', 'pear']
+frutes.pop();
+
+console.log(frutes); // ['apple', 'banana', 'orange', 'grape', 'kiwi']
+
+frutes.unshift('pear');
+console.log(frutes); // ['pear', 'apple', 'banana', 'orange', 'grape', 'kiwi']
+frutes.shift();
+console.log(frutes); // ['apple', 'banana', 'orange', 'grape', 'kiwi']
