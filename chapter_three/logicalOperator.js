b7994bfef9f3ad32eae2e377c0c918b8e85ce8fb
Seemol Chakroborti <wpseemol>
2025-03-26 12:21:17 +0600

    setting set on vscode word shop

---
+++ chapter_three/logicalOperator.js
@@ -0,0 +1,24 @@
+// logicalOparetor in js
+
+// && or || oparetor
+
+let helo = 'some text here';
+
+/**
+ *  && oparetor
+ *
+ * true && true = true
+ * false && true = false
+ * true && false = false
+ * false && false = false
+ *
+ * || oparetor
+ *
+ * true || true = true
+ * false || true = true
+ * true || false = true
+ * false || false = false
+ *
+ *
+ *
+ */
