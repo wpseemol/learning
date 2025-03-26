7e8e7dbe5fcbee69168d9207f641bfb9a9e61a15
Seemol Chakroborti <wpseemol>
2025-03-27 00:40:53 +0600

    learning chpataer remove

--- insertRemove.js
+++ insertRemove.js
@@ -12,3 +12,6 @@ frutes.unshift('pear');
 console.log(frutes); // ['pear', 'apple', 'banana', 'orange', 'grape', 'kiwi']
 frutes.shift();
 console.log(frutes); // ['apple', 'banana', 'orange', 'grape', 'kiwi']
+
+frutes.splice(2, 0, 'pear');
+console.log(frutes); // ['apple', 'banana', 'pear', 'orange', 'grape', 'kiwi']  
