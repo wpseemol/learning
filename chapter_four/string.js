649040d6c47e7bc076157306bc83c0935a6f5372
Seemol Chakroborti <wpseemol>
2025-03-26 14:33:43 +0600

    string

---
+++ chapter_four/string.js
@@ -0,0 +1,6 @@
+/**
+ * The string object is used to represent and manipulate a sequence of characters.
+ * 
+ */
+
+// string reference:- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
