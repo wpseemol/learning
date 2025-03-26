33592979e67dd5a50316c5feed7b2ae2e664bb85
Seemol Chakroborti <wpseemol>
2025-03-26 19:30:11 +0600

    multeple type array can store hare

---
+++ chapter_five/arra.js
@@ -0,0 +1,6 @@
+/**
+ * Array is data structure where we can organize multiple data under a single variable.
+ */
+
+const arra = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; // array literal
+const arra2 = Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10); // array constructor
