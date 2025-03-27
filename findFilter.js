a8b9a2ca56b348c821a3f0c60f0cf1a716bba52e
Seemol Chakroborti <wpseemol>
2025-03-27 16:54:30 +0600

    my create function

---
+++ findFilter.js
@@ -0,0 +1,30 @@
+
+
+const array = [1,2,3,4,5];
+
+const resultFind = array.find((item) => item === 3);
+const resultFilter = array.filter((item) => item === 3);
+
+console.log("js builten \n","find:",resultFind,"filter:", resultFilter); 
+
+function myFilter(array, callback) {
+    for (let i = 0; i < array.length; i++) {
+        if (callback(array[i], i, array)) {
+            return [array[i]];
+        }
+    }
+}
+
+function myFind(array, callback) {
+    for (let i = 0; i < array.length; i++) {
+        if (callback(array[i], i, array)) {
+            return array[i];
+        }
+    }
+}
+
+const myFilterResult = myFilter(array, (item) => item === 3);
+console.log("my create \n","my filter:", myFilterResult);
+
+const myFindResult = myFind(array, (item) => item === 3);
+console.log("my create \n","my find:", myFindResult);
