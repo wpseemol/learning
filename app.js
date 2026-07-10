b2052af0bd9b25e91c2c139ae699b2601c7ac174
wpseemol <wpseemol@gmail.com>
2026-07-11 02:19:26 +0600

    index file pass thro file system

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const http = require("http");
+const fs = require("fs");
+const path = require("path");
+const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/html");
-    res.end("<h1>Hello World</h1>");
+    const filePath = path.join(__dirname, "index.html");
+    fs.readFile(filePath, "utf8", (err, data) => {
+        if (err) {
+            res.statusCode = 500;
+            res.setHeader("Content-Type", "text/plain");
+            res.end("Error reading file");
+        } else {
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "text/html");
+            res.end(data);
+        }
+    });
 });
-const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}/`);
